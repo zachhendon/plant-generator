db903d1e852f70807c0b5d4b977b35b63484d559
ZachHendon <zhendon@wisc.edu>
2024-03-17 21:14:52 -0500

    Setup three.js for drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Canvas} from '@react-three/fiber'
+import {Line} from "@react-three/drei"
+import {Vector2} from "three"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const points: Array<Vector2> = [];
+  points.push(new Vector2(0, 0))
+  points.push(new Vector2(1, 0))
+  points.push(new Vector2(1, 1))
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Plant Generator</h1>
+      <Canvas>
+        <Line
+          points={points}
+          lineWidth={2}
+        />
+      </Canvas>
     </>
   )
 }
